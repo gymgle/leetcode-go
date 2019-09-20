0b64b84c31b4b0d043fc73a392268145f7169846
Yaming Gong <ymgongcn@gmail.com>
2019-09-20 12:22:34 +0800

    Add two sum tests and benchmark tests

--- two-sum/two-sum_test.go
+++ two-sum/two-sum_test.go
@@ -4,31 +4,54 @@ import (
 	"testing"
 )
 
-func Test_twoSum(t *testing.T) {
-	tests := [][]int{
-		[]int{0, 1},
-		[]int{1, 2, 3},
-		[]int{2, 7, 11, 15},
-	}
+var tests = [][]int{
+	[]int{0, 1},
+	[]int{1, 2, 3},
+	[]int{2, 7, 11, 15},
+}
 
-	targets := []int{
-		1,
-		4,
-		22,
-	}
+var targets = []int{
+	1,
+	4,
+	22,
+}
 
-	results := [][]int{
-		[]int{0, 1},
-		[]int{0, 2},
-		[]int{1, 3},
-	}
+var results = [][]int{
+	[]int{0, 1},
+	[]int{0, 2},
+	[]int{1, 3},
+}
 
-	caseNum := 3
+var caseNum = len(targets)
+
+func TestTwoSum1(t *testing.T) {
 	for i := 0; i < caseNum; i++ {
-		if ret := twoSum(tests[i], targets[i]); ret[0] != results[i][0] || ret[1] != results[i][1] {
+		if ret := twoSum1(tests[i], targets[i]); ret[0] != results[i][0] || ret[1] != results[i][1] {
 			t.Error("test failed")
 		} else {
 			t.Log("test passed")
 		}
+	}
+}
+
+func TestTwoSum2(t *testing.T) {
+	for i := 0; i < caseNum; i++ {
+		if ret := twoSum2(tests[i], targets[i]); ret[0] != results[i][0] || ret[1] != results[i][1] {
+			t.Error("test failed")
+		} else {
+			t.Log("test passed")
+		}
+	}
+}
+
+func BenchmarkTwoSum1(b *testing.B) {
+	for i := 0; i < caseNum; i++ {
+		twoSum1(tests[i], targets[i])
+	}
+}
+
+func BenchmarkTwoSum2(b *testing.B) {
+	for i := 0; i < caseNum; i++ {
+		twoSum2(tests[i], targets[i])
 	}
 }
