07976916ee6f06f53a053e8d43d97fca69449e32
Yaming Gong <ymgongcn@gmail.com>
2019-09-20 21:48:15 +0800

    Add reverse linked list recursion solution

--- reverse-linked-list/reverse-linked-list.go
+++ reverse-linked-list/reverse-linked-list.go
@@ -8,6 +8,8 @@ package leetcode
  * Example:
  * Input: 1->2->3->4->5->NULL
  * Output: 5->4->3->2->1->NULL
+ *
+ * https://leetcode.com/problems/reverse-linked-list/
  */
 
 // 主要考察翻转的写法，用 go 语言可以精简到一行，不需要 c/c++ 使用中间变量
@@ -18,6 +20,7 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// 迭代解法
 func reverseList(head *ListNode) *ListNode {
 	cur := head
 	var pre *ListNode
@@ -31,4 +34,28 @@ func reverseList(head *ListNode) *ListNode {
 		*/
 	}
 	return pre
+}
+
+/**
+ * 递归解法
+ * 对于每一个链表节点，我们考虑先将其后续的链表进行反转。
+ *
+ * 1->2->3->4->5->NULL 假设我们已经将后面两个节点反转了得到了
+ * 1—>2->3->4<-5
+ *      cur    p
+ * 那么我们执行cur->next->next = cur,cur->next = NULL可以得到
+ * 1—>2->3<-4<-5
+ * 递归进行即可
+ */
+
+func reverseList2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	newHead := reverseList2(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+
+	return newHead
 }
