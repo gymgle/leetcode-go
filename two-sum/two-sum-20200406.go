730df3428258b3dfb61ab8e8e557285ce4f76d81
Yaming Gong <ymgongcn@gmail.com>
2020-04-06 18:56:05 +0800

    Add two sum

---
+++ two-sum/two-sum-20200406.go
@@ -0,0 +1,25 @@
+/**
+ * 1.两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * https://leetcode-cn.com/problems/two-sum
+ */
+
+package leetcode
+
+func twoSum3(nums []int, target int) []int {
+	if len(nums) < 1 {
+		return nil
+	}
+
+	m := make(map[int]int)
+	for i, v := range nums {
+		if _, exist := m[target-v]; exist {
+			return []int{m[target-v], i}
+		}
+		m[v] = i
+	}
+
+	return nil
+}
