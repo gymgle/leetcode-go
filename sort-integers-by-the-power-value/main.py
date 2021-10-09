ce84dd5e030b282b48b829ce67c597a18505036c
Gymgle <ymgongcn@gmail.com>
2021-10-09 02:39:23 +0000

    refactor: use memo

--- sort-integers-by-the-power-value/main.py
+++ sort-integers-by-the-power-value/main.py
@@ -9,18 +9,23 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        memo = dict()
+
+        def _power(n):
+            if n in memo:
+                return memo[n]
+
+            if n == 1:
+                return 0
+            if n % 2 == 0:
+                memo[n] = _power(n / 2) + 1
+            else:
+                memo[n] = _power(n * 3 + 1) + 1
+            return memo[n]
+
         v = list(range(lo, hi + 1))
         v.sort(key=lambda x: (_power(x), x))
         return v[k - 1]
-
-
-def _power(n):
-    if n == 1:
-        return 0
-    if n % 2 == 0:
-        return _power(n / 2) + 1
-    else:
-        return _power(n * 3 + 1) + 1
 
 
 if __name__ == '__main__':
