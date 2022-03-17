76ed00a6d27addf318d5b1cf310dbb5d5f2bba96
Gymgle <ymgongcn@gmail.com>
2022-03-17 20:07:18 +0800

    feat: add implement strStr

--- implement-strstr/sol.go
+++ implement-strstr/sol.go
@@ -1,1 +1,16 @@
-package implement_strstr
+package main
+
+import "strings"
+
+func strStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+	if haystack == "" {
+		return -1
+	}
+	if len(haystack) < len(needle) {
+		return -1
+	}
+	return strings.Index(haystack, needle)
+}
