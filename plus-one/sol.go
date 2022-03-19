539826078ec9867b436198dc668e35f0d04b6b3c
Gymgle <ymgongcn@gmail.com>
2022-03-19 18:48:38 +0800

    feat: add plus one

--- plus-one/sol.go
+++ plus-one/sol.go
@@ -1,1 +1,16 @@
-package plus_one
+package main
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] == 9 {
+			digits[i] = 0
+		} else {
+			digits[i]++
+			break
+		}
+	}
+	if digits[0] == 0 {
+		digits = append([]int{0}, digits...)
+	}
+	return digits
+}
