e7d8b47a62a3f8c55755054fffa74950ca8d5ddc
Gymgle <ymgongcn@gmail.com>
2022-03-25 21:18:24 +0800

    feat: add two city scheduling

---
+++ two-city-scheduling/sol.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key=lambda x: x[0] - x[1])
+
+        total = 0
+        n = len(costs) // 2
+        for i in range(n):
+            total += costs[i][0] + costs[n + i][1]
+        return total
