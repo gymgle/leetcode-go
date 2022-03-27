25b84cbcf9996e442b9c2c6ee92a68b498b1eba5
Gymgle <ymgongcn@gmail.com>
2022-03-27 20:34:49 +0800

    feat: add second highest salary

--- sql/second-highest-salary.sql
+++ sql/second-highest-salary.sql
@@ -1,6 +1,14 @@
 # https://leetcode.com/problems/second-highest-salary
 
+# sol1
 SELECT (SELECT DISTINCT salary
         FROM Employee
         ORDER BY salary DESC
         LIMIT 1, 1) AS `SecondHighestSalary`;
+
+
+# sol2
+SELECT IFNULL((SELECT DISTINCT salary
+               FROM Employee
+               ORDER BY salary DESC
+               LIMIT 1, 1), NULL) AS `SecondHighestSalary`;
